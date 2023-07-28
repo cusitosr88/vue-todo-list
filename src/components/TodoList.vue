c959112834c92c569d110d0e8b30c2501a452b6f
Javier Suarez <cusitosr88@gmail.com>
2023-07-28 13:14:15 -0400

    functions for local storage

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <script lang='ts' setup>
 import ListItem from './ListItem.vue'
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import type { Ref } from 'vue'
 
 type Item = {
@@ -8,21 +8,45 @@ type Item = {
   checked?: boolean
 }
 
-const listItems: Ref<Item[]> = ref([
-  { title: 'Make a todo list app', checked: true },
-  { title: 'Predict the weather', checked: false },
-  { title: 'Play some tunes', checked: false },
-  { title: 'Let\'s get cooking', checked: false },
-  { title: 'Pump some iron', checked: false },
-  { title: 'Track my expenses', checked: false },
-  { title: 'Organize a game night', checked: false },
-  { title: 'Learn a new language', checked: false },
-  { title: 'Publish my work' }
-])
+const storageItems: Ref<Item[]> = ref([])
+
+const setToStorage = (items: Item[]): void => {
+  localStorage.setItem('list-items', JSON.stringify(items))
+}
+
+const getFromStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem('list-items')
+  if (stored) {
+    return JSON.parse(stored)
+  }
+  return []
+}
+
+if (storageItems.value?.length === 0) {
+  const listItems: Item[] = [
+    { title: 'Make a todo list app', checked: true },
+    { title: 'Predict the weather', checked: false },
+    { title: 'Play some tunes', checked: false },
+    { title: "Let's get cooking", checked: false },
+    { title: 'Pump some iron', checked: false },
+    { title: 'Track my expenses', checked: false },
+    { title: 'Organise a game night', checked: false },
+    { title: 'Learn a new language', checked: false },
+    { title: 'Publish my work', checked: false },
+  ];
+  setToStorage(listItems);
+  storageItems.value = listItems;
+}
 
 const sortedList = computed(() =>
-    [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
-)
+  [...storageItems.value].sort(
+    (a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0)
+  )
+);
+
+onMounted(() => {
+  storageItems.value = getFromStorage()
+})
 
 const updateItem = (item: Item): void => {
   const updatedItem = findItemInList(item)
@@ -30,11 +54,12 @@ const updateItem = (item: Item): void => {
     toggleItemChecked(updatedItem)
   }
 }
+
 const findItemInList = (item: Item): Item | undefined => {
-  return listItems.value.find(
+  return storageItems.value.find(
     (itemInList: Item) => itemInList.title === item.title
-  )
-}
+  );
+};
 
 const toggleItemChecked = (item: Item): void => {
   item.checked = !item.checked
