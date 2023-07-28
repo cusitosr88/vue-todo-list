d3fc52e31879a365d975779765441e027a203789
Javier Suarez <cusitosr88@gmail.com>
2023-07-28 12:25:57 -0400

    TodoLIst component reactivity

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,14 @@
 <script lang='ts' setup>
 import ListItem from './ListItem.vue'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
 
 type Item = {
   title: string,
   checked?: boolean
 }
 
-const listItems: Item[] = [
+const listItems: Ref<Item[]> = ref([
   { title: 'Make a todo list app', checked: true },
   { title: 'Predict the weather', checked: false },
   { title: 'Play some tunes', checked: false },
@@ -16,7 +18,23 @@ const listItems: Item[] = [
   { title: 'Organize a game night', checked: false },
   { title: 'Learn a new language', checked: false },
   { title: 'Publish my work' }
-]
+])
+
+const updateItem = (item: Item): void => {
+  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    toggleItemChecked(updatedItem)
+  }
+}
+const findItemInList = (item: Item): Item | undefined => {
+  return listItems.value.find(
+    (itemInList: Item) => itemInList.title === item.title
+  )
+}
+
+const toggleItemChecked = (item: Item): void => {
+  item.checked = !item.checked
+}
 </script>
 
 <template>
@@ -25,7 +43,16 @@ const listItems: Item[] = [
       :key='key'
       v-for='(item, key) in listItems'
     >
-      <ListItem :is-checked='item.checked'>{{ item.title }}</ListItem>
+    <ListItem :is-checked='item.checked' v-on:click.prevent="updateItem(item)">{{ item.title }}</ListItem>
     </li>
   </ul>
 </template>
+<style scoped>
+ul {
+  list-style: none;
+}
+
+li {
+  margin: 0.4rem 0;
+}
+</style>
